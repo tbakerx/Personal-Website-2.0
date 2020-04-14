6e550693f3a48cdd8afe300f5d716adfa40bea58
Tim Baker <tbakerx@gmail.com>
2020-04-13 19:15:35 -0700

    Re-initialize from my NextJs starter template

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Personal-Website-2.0
-This is a NextJs + Express version of my personal website
+# NextJs + Express + Mobx + Typescript Starter
--- pages/index.js
+++
@@ -1,6 +0,0 @@
-const Index = () => (
-    <div>
-        <h1>Testing out the deploy of my new Next app</h1>
-    </div>
-)
-export default Index
